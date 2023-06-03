7030a4ed128407fb53646881c546ee0c4481a640
Mansi <mansisharma209aps@gmail.com>
2023-06-03 23:00:54 +0530

    first commit

---
+++ foodhub/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg
+import React from 'react';
+
+import './App.css';
+import Header from './shared/Header';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
